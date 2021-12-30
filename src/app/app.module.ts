ecca399302eb70a3f35b9b4a21fca8440b678909
karthik <karthik.irax@gmail.com>
2021-12-30 18:50:11 +0530

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { HeaderComponent } from './header.component';
+import { FooterComponent } from './footer.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,117 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+
+
+  registrationForm = new FormGroup({
+    
+    Employeeid : new FormControl(''),
+    Employeename : new FormControl(null,[Validators.required]),
+    Designation: new FormControl(''),
+    Salary : new FormControl(null,[Validators.required,Validators.pattern(/^[1-9]\d*(\.\d+)?$/)]),
+    Email : new FormControl(null,[Validators.required,Validators.email]),
+    Mobile : new FormControl(null,[Validators.required,Validators.minLength(10),Validators.maxLength(10),Validators.pattern(/^[6-9][0-9]{0,9}$/)]),
+    Gender : new FormControl('',[Validators.required]),
+    Qualification: new FormControl(''),
+
+    
+
+  });
+
+
+get Employeeid(){
+  return this.registrationForm.get('Employeeid');
+}
+get Employeename(){
+  return this.registrationForm.get('Employeename');
+}
+
+get Designation(){
+  return this.registrationForm.get('Designation');
+}
+
+get Salary(){
+  return this.registrationForm.get('Salary');
+}
+get Email(){
+  return this.registrationForm.get('Email');
+}
+get Mobile(){
+  return this.registrationForm.get('Mobile');
+}
+get Gender(){
+  return this.registrationForm.get('Gender');
+}
+get Qualification(){
+  return this.registrationForm.get('Qualification');
+}
+//,english:'',hindi:'',telugu:',
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  lstUsers:any = [];
+  newLstUser:any = [];
+
+  user_info:any = {
+    Employeeid:'',Employeename:'',Designation:'',Salary:'',Email:'',Mobile:'',Gender:'',Qualification:'',
+
+
+
+  }
+
+  constructor() { }
+
+  ngOnInit(){
+    this.newLstUser = JSON.parse(localStorage.getItem('$key') || '') ;
+  }
+
+
+  doAddUser(){
+    // console.log('user_info', this.user_info);
+    if(this.user_info){
+        this.newLstUser.push(this.user_info);
+        localStorage.setItem('$key',JSON.stringify(this.newLstUser));
+        this.user_info= {};   
+        window.location.reload();
+        console.log(this.user_info);       
+    }
+  }
+
+  doDelate(index:any){
+    if(index > -1 ){
+      this.newLstUser.splice(index,1);
+      localStorage.setItem('$key',JSON.stringify(this.lstUsers));
+
+    }
+  }
+
+  doDelateAll(){
+    localStorage.clear();
+    
+      window.location.reload();
+  
+  }
+
+
+
+
+}
